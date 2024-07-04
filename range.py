b7fa441438aae1408139f4f69723ecdbf77f5f5b
haseebahmedWD <haseebahmed23071991@gmail.com>
2024-07-04 14:40:59 +0500

    list updated range updated

--- range.py
+++ range.py
@@ -4,20 +4,20 @@
 #write a program to print square of natural numbers using range
 num = int(input("Enter number range: "))
 for x in range(1,num+1):
-    print(x*x)
-
+    print(x*x, end=' ')
+print('\n')
 #print range values with single argument
 #start = 0 step = 1 by default
 r = range(5)
 for x in r:
     print(x, end=" ")
 print('\n')
-
+#print the range with 2 steps
 r = range(2,10,2)
 for x in r:
     print(x, end=" ")
 print('\n')
-
+#print the range with -2 steps
 r = range(10,3,-2)
 for x in r:
     print(x, end=" ")
